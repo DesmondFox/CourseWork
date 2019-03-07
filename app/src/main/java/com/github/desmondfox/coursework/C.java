83b0e201d4346a0e142b369fbbcdf6ea71d97795
Alexey Rott <survare@hotmail.com>
2019-03-07 22:14:04 +0200

    added class for constants

---
+++ app/src/main/java/com/github/desmondfox/coursework/C.java
@@ -0,0 +1,8 @@
+package com.github.desmondfox.coursework;
+
+import android.os.Build;
+
+public class C {
+    public static boolean C_KITKAT = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+    public static boolean C_NOUGAT = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1;
+}
